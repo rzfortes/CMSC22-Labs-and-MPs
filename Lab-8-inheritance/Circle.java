e3dccc72593c683f138b4a846ae13760790f0968
Ricafelle Dorothy Z. Fortes <rzfortes@up.edu.ph>
2016-10-05 11:34:21 +0800

    updates exercise part 1 by rica please review christine
    not sure sa square class

--- Lab-8-inheritance/Circle.java
+++ Lab-8-inheritance/Circle.java
@@ -1,38 +1,47 @@
-import java.lang.Math;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
 
-class Circle extends Shape{
-	private double radius = 1.0;
 
-	public Circle(){
-		radius = 1.0;
-	}
-
-	public Circle(double radius){
-		this.radius = radius;
-	}
-
-	public Circle(double radius, String color, boolean filled){
-		super(color, filled);
-		this.radius = radius;
-	}
-
-	public double getRadius(){
-		return radius;
-	}
-
-	public void setRadius(double radius){
-		this.radius = radius;
-	}
-
-	public double getArea(){
-		return Math.PI * Math.pow(radius, 2);
-	}
-
-	public double getPerimeter(){
-		return 2 * Math.PI * radius;
-	}
-
-	public String toString(){
-		System.out.println("");
-	}
+public class Circle extends Shape {
+    private double radius = 1.0;
+    
+    public Circle() {
+        this.radius = 1.0;
+    }
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+    
+    public double getRadius() {
+        return radius;
+    }
+    
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+    
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    
+    @Override
+    public String toString() {
+        return "A circle with radius = " + radius + "which is a subclass of " + 
+                super.toString();
+    }
+    
 }
--- Lab-8-inheritance/Shape.java
+++ Lab-8-inheritance/Shape.java
@@ -1,42 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
 
+// rica's lab 8 cmsc 22
 
-class Shape{
-	String color = new String("red");
-	boolean filled = true;
-
-	public Shape(){
-		color = "green";
-	}
-
-	public Shape(String color, boolean filled){
-		this.color = color;
-		this.filled = filled;
-	}
-
-	public String getColor(){
-		return color;
-	}
-
-	public void setColor(String color){
-		this.color = color;
-	}
-
-	public boolean isFilled(){
-		return filled;
-	}
-
-	public void setFilled(boolean filled){
-		this.filled = filled;
-	}
-
-	public String toString(){
-		
-		String ans = "";
-		if(isFilled()){
-			ans = "filled";
-		}else{
-			ans = "not filled";
-		}
-		return String.format("A Shape with color of " + color + " and " + ans + ".");
-	}
+public class Shape {
+    
+    private String color = "red";
+    boolean filled = true;
+    
+    public Shape() {
+        color = "green";
+        // consult if kinahanglan pa ni siya i-initialize balik
+        filled = true;
+    }
+    
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+    
+    public String getColor() {
+        return color;
+    }
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+    
+    public boolean isFilled() {
+        return filled;
+    }
+    
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+    
+    @Override
+    public String toString() {
+        String ans;
+        if(isFilled()) {
+            ans = "filled";
+        } else {
+            ans = "not filled";
+        }
+        return String.format("A Shape with color of %s is %s.", color, ans);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
 }
