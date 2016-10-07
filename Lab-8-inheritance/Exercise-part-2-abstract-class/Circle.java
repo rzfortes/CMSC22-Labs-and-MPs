ea3c074a96c11ef1b32a6f0570846f7b14c90ca4
Ricafelle Dorothy Z. Fortes <rzfortes@up.edu.ph>
2016-10-07 21:29:45 +0800

    documentation added

--- Lab-8-inheritance/Exercise-part-2-abstract-class/Circle.java
+++ Lab-8-inheritance/Exercise-part-2-abstract-class/Circle.java
@@ -1,14 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * created by Christine Catubig and Ricafelle Fortes, 10/5/2016 - 10/7/2016
+ * 
  */
+
 package inheritance2;
 
-/**
- *
- * @author rdfortes
- */
+
 public class Circle extends Shape {
     
     private double radius = 1.0;
