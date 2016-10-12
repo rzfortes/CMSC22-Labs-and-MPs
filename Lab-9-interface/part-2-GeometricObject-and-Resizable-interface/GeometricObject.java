c936180c9274b1499e5efa8f9804fbef4fc71f64
Ricafelle Dorothy Z. Fortes <rzfortes@up.edu.ph>
2016-10-12 17:43:52 +0800

    geometric object interface

---
+++ Lab-9-interface/part-2-GeometricObject-and-Resizable-interface/GeometricObject.java
@@ -0,0 +1,13 @@
+/*
+ * created by Christine Catubig and Rica Fortes, 10/11/2016 - 10/12/2016
+ *
+ */
+package pkginterface;
+
+
+public interface GeometricObject {
+    
+    double getPerimeter();
+    double getArea();
+    
+}
