e9580cfbb9a768a1943f7439b44653582d768640
Ricafelle Dorothy Z. Fortes <rzfortes@up.edu.ph>
2016-11-05 15:23:26 +0800

    Update Course.java

--- Lab-13-studentdb2/Course.java
+++ Lab-13-studentdb2/Course.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentdb2;
+
+/**
+ *
+ * @author rdfortes, modified 3:18 pm, 11/5/2016
+ */
 import java.io.Serializable;
 
 
